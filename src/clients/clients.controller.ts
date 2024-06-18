e46411d2316208da1e389f348467b69e5ceff4e2
kecsou <kevin.voyer.developpeur@gmail.com>
2024-06-18 14:27:54 +0200

    feat: add find all method to the client controller

--- src/clients/clients.controller.ts
+++ src/clients/clients.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Param, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 import { CreateClientDto } from './dto/create-client.dto';
 import { User } from 'src/authent/user.decorator';
 import { Client } from 'src/store/store.provider';
@@ -10,12 +10,16 @@ export class ClientsController {
     constructor(private readonly storeService: StoreService) {}
 
     @Post()
-    create(@Body() createClientDto: CreateClientDto) {
+    create(@Body() createClientDto: CreateClientDto): Client {
         // Check that no user already exists with this username or id
+        // Add the client within the store
+        // Return the created client instead of null
+
+        return null
     }
 
     @Patch(':clientId')
-    update(@User() user: Client, @Param('clientId') clientId: string, updateClientDto: UpdateClientDto) {
+    update(@User() user: Client, @Param('clientId') clientId: string, updateClientDto: UpdateClientDto): Client {
         // should check that the current client is the one targetted, if not throw a forbidden exception (http code 403)
         // Actually replace the client infos
         // Return the updated client instead of null
@@ -29,5 +33,11 @@ export class ClientsController {
         // If not throw a forbidden exception
         // Otherwise remove the user from the storage
         // This function does not require to remove anything
+    }
+
+    @Get()
+    findAll(): Client[] {
+        // Return the entire list of client instead of an empty array
+        return [];
     }
 }
